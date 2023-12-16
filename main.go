cd4b2a586e6b1f4a4d826114d7bffe9c42733415
Yarince Padilla <padilla.yarince@gmail.com>
2023-12-16 02:57:16 -0600

    add validations

--- main.go
+++ main.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"database-go-with-repository-pattern/database"
-	"database-go-with-repository-pattern/repository"
-	"log"
-
 	"database/sql"
+	"log"
 )
 
 var Connection *sql.DB
+var err error
+
+// RequestedId Variable just for the example
+var RequestedId int32 = 1
 
 func main() {
-
-	Connection = database.GetConnection()
-	commentRepository := repository.NewCommentRepository(Connection)
-	c, err := repository.GetComment(commentRepository)
+	Connection, err = database.GetConnection()
 	if err != nil {
-		log.Println("Error on retrieving a comment: ", err)
+		log.Println("Error acquiring the database connection:", err)
 	}
-	log.Printf("Comment %+v", c)
+
+	defer func() {
+		err := database.CloseConnection()
+		if err != nil {
+			log.Println("Error closing the database connection:", err)
+		}
+	}()
+
+	SearchDefaultComment()
 }
